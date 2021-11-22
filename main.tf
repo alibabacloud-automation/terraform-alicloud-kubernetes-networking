234dd3bcdd8530c0cec09472b5d2bf2e056fb3d6
liuyuan <709503583@qq.com>
2021-11-22 13:38:23 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-// Provider specific configs
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/kubernetes-networking"
-}
-
 locals {
   vpc_name         = var.vpc_name == "" ? "tf-k8s-networking-module-vpc" : var.vpc_name
   vswitch_name     = var.vswitch_name == "" ? "tf-k8s-networking-module-vsw" : var.vswitch_name
